9379cb9353e9a387780a5760765ec4ac4a63e29b
Néo Ascalon <ignatdominic@yahoo.com>
2019-11-29 15:08:34 +0100

    Alpha 1.0

--- C_S_NODE_MCU.ino
+++ C_S_NODE_MCU.ino
@@ -8,6 +8,7 @@
 #include <HardwareSerial.h>
 #include <string>
 
+#define SendKey 0
 
 const char* ssid = "Point4";      // SSID
 const char* password = "PointAccess4";      // Password
@@ -16,20 +17,25 @@ const char* ID_Carte = "01";
 const char* ID_Lam1 = "L01";
 const char* ID_Lam2 = "L02";
 const int   port = 9000;            // Port serveur - Server Port
-int pin_led1 = 5;
-int pin_led2 = 4;
-int pin_bouton1 = 12;
-int pin_bouton2 = 15;
+int pin_led1 = 2; //D4
+int pin_led2 = 4; //D2
+int pin_bouton1 = 12; //D6
+bool AP_1;
+int pin_bouton2 = 15; //D8
+bool AP_2;
 
 void Reconnection(); //If signal to server is lost
-void ServerPrint(); //Send to server
-void ServerRead(); //Read from server
+void Server_Read(); //Waiting for client and Read directives from Raspberry server
+void Client_Write(); //Respond to server (optional)
 void Deconnetion(); //Disconect from Server
-
+void Command_Decript_Execute();
+void Controle_Proche(); //Controle par les boutons physiques
 
 String Message = (String)NULL;
-WiFiClient Server;
-WiFiClient ServerPresenceTest;
+WiFiClient Client;
+WiFiServer Server(port);
+
+int count = 0;
 
 
 void setup() // the setup function runs once when you press reset or power the board
@@ -39,10 +45,14 @@ void setup() // the setup function runs once when you press reset or power the b
 
 	pinMode(pin_led1, OUTPUT);
 	pinMode(pin_led2, OUTPUT);
+
 	pinMode(pin_bouton1, INPUT);
 	pinMode(pin_bouton2, INPUT);
+
 	digitalWrite(pin_led1, LOW);
+	AP_1 = LOW;
 	digitalWrite(pin_led2, LOW);
+	AP_2 = LOW;
 
 	Serial.print("Connecting to ");
 	Serial.println(ssid);
@@ -62,69 +72,72 @@ void setup() // the setup function runs once when you press reset or power the b
 	Serial.print("Connected To Network, IP address: ");
 	Serial.println(WiFi.localIP());
 
-	Serial.print("Connecting to Server..");
-
-	while (!Server.connected())
-	{
-		delay(500);
-		Serial.print(".");
-		Server.connect(host, port);
-	}
-	Serial.println();
-	Serial.println("Connected to Server");
+	Server.begin();
+	Serial.print("Open Client and connect to IP:");
+	Serial.print(WiFi.localIP());
+	Serial.print(" on port ");
+	Serial.println(port);
 
 }
 
 void loop()
 {
-	Reconnection();
-	ServerRead();
+	Server_Read();
+
 	Command_Decript_Execute();
-	//ServerPrint();
-	delay(10000);
+	
+	Controle_Proche();
+	
+	delay(500);
 }
 
 void Reconnection()
 {
-	if (!ServerPresenceTest.connect(host, port)) {
-		Serial.print("Connection To Server Lost,Reconecting..");
-		Server.stop();
-		while (!Server.connected())
+	if (!Client.connected()) {
+
+		Serial.print("Client Lost Connection,Reconecting..");
+		Client.stop();
+
+		while (!Client.connected())
 		{
 			Serial.print(".");
-			Server.connect(host, port);
+			Client.connect(host, port);
 			delay(500);
 		}
 
 		Serial.println();
-		Serial.println("Connected to Server");
-
-	}
-	ServerPresenceTest.stop();
-}
-
-void ServerPrint()
-{
-	Message = Serial.readString();
-	if (Message == (String)NULL) {
-		Serial.println(Message);
-		Server.print("Le Message recu est: " + Message );
+		Serial.println("Client Connected");
 	}
 }
 
-void ServerRead()
+void Server_Read()
 {
-	if (Server.available())
+	Client = Server.available();
+
+	if (Client)
 	{
-		Message = Server.readStringUntil('\n');
+		if (Client.connected() && (Client.available() > 0))
+		{
+			Serial.println("Client Connected");
+			Message = Client.readStringUntil('\n');  // read data from the connected client
+			Serial.println(Message);
+		}
+	}
+}
+
+void Client_Write()
+{
+	if (Client.connected())
+	{
+		Message = Client.readStringUntil('\n');
 	}
 }
 
 void Deconnetion()
 {
-	if (Server.connected())
+	if (Client.connected())
 	{
-		Server.stop();
+		Client.stop();
 	}
 }
 
@@ -146,23 +159,45 @@ void Command_Decript_Execute()
 		if (Message == "L01A")
 		{
 			digitalWrite(pin_led1, HIGH);
+			AP_1 = HIGH;
 		}
 		else if(Message == "L01E")
 		{
 			digitalWrite(pin_led1, LOW);
+			AP_1 = LOW;
 		}
 
 
 		if (Message == "L02A")
 		{
 			digitalWrite(pin_led2, HIGH);
+			AP_2 = HIGH;
 		}
 		else if (Message == "L02E")
 		{
 			digitalWrite(pin_led2, LOW);
+			AP_2 = LOW;
 		}
 
 
 	}
 	Message = (String)NULL;
+}
+
+void Controle_Proche()
+{
+	if (digitalRead(pin_bouton1) == HIGH)
+	{
+		Serial.println("Switch1");
+		AP_1 = (!AP_1);
+		digitalWrite(pin_led1, AP_1);
+	}
+
+	if (digitalRead(pin_bouton2) == HIGH)
+	{
+		Serial.println("Switch2");
+		AP_2 = (!AP_2);
+		digitalWrite(pin_led2, AP_2);
+
+	}
 }
